61efba10eb1938a1592e98f70b6b6579986853ec
Raycis <raycis_adm@mail.ru>
2020-05-27 16:48:13 +0300

    Create index

---
+++ precache-manifest.af4bf9374bcc0427ed8df7b72fd904cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c331d4b19b126d5e92764b697b8255",
+    "url": "/social_network/index.html"
+  },
+  {
+    "revision": "f1b8cfb161ecc1218e3c",
+    "url": "/social_network/static/css/main.5b8c8ba7.chunk.css"
+  },
+  {
+    "revision": "e999f23de1aded3c2979",
+    "url": "/social_network/static/js/2.fda7e36e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social_network/static/js/2.fda7e36e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1b8cfb161ecc1218e3c",
+    "url": "/social_network/static/js/main.8f67fd22.chunk.js"
+  },
+  {
+    "revision": "6f3adbd228d265230240",
+    "url": "/social_network/static/js/runtime-main.98ecd959.js"
+  },
+  {
+    "revision": "de470447f4fe98efd7e02bccb5b42bae",
+    "url": "/social_network/static/media/avatar.de470447.png"
+  }
+]);
